6513656c2a493dc66e7aa20a66de24b9be38f562
Michael Goetting <mgoettin@techfak.uni-bielefeld.de>
2018-10-12 13:17:58 +0200

    Init uebung1

---
+++ src/test/java/de/techfak/gse/template/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package de.techfak.gse.template;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+class ExampleUnitTest {
+
+    @Test
+    void unitTest() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
