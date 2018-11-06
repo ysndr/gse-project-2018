d0bf9d2aec2184d8858297b34b121dc25f91bb24
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-11-06 20:12:45 +0100

    Set up controller to handle game flow (refs: #51305, #51307, #51297, #51298)

--- src/main/java/de/techfak/gse/ysander/controller/ChessController.java
+++ src/main/java/de/techfak/gse/ysander/controller/ChessController.java
@@ -2,32 +2,49 @@ package de.techfak.gse.ysander.controller;
 
 import de.techfak.gse.ysander.model.Move;
 import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.error.ChessGameException;
 import de.techfak.gse.ysander.model.error.InvalidMoveException;
 import de.techfak.gse.ysander.view.MovesInput;
 import de.techfak.gse.ysander.view.Output;
+import de.techfak.gse.ysander.view.RawInput;
+import de.techfak.gse.ysander.view.View;
 
 import java.util.List;
 import java.util.stream.Collector;
 
-public class ChessController {
+public class ChessController<V extends View<ChessGameException> & RawInput> {
 
-    private MovesInput movesInput;
-    private Output<State> view;
 
-    public ChessController updateView(State state) {
-        view.display(state);
-        return this;
+    private State state;
+
+    public ChessController(V view, State startState) {
+
+        state = startState;
+
+        view.setExceptionCB((e, o) -> {
+            o.display(state);
+            System.exit(e.getErrorCode());
+        });
+
+        view.setRawInputCB((i, o) -> {
+            String[] moves = i.trim().split(";");
+            for (String moveStr : moves) {
+                Move move = Move.fromString(moveStr);
+                state = state.applyMove(move);
+            }
+            o.display(state);
+        });
+
+        view.setOnInitCB((o) -> {
+            o.display(state);
+        });
     }
 
-    public void turn(State state) {
-
-        List<Move> moves = movesInput.requestMoves();
-
-        for (Move move : moves) {
-            state = state.applyMove(move);
-        }
-
+    public ChessController(V view) {
+        this(view, State.defaultState());
     }
+
+
 
 
 }
