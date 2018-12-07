a9859c32bcd7640cbe8c80266e4c27d487210e09
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-07 16:55:29 +0100

    Implement checked colors and button sizing (refs: #54389, @0h30m)

--- src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessTile.java
+++ src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessTile.java
@@ -3,11 +3,19 @@ package de.techfak.gse.ysander.view.gui.elements;
 import java.io.IOException;
 
 import javafx.beans.NamedArg;
-import javafx.beans.property.*;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 import de.techfak.gse.ysander.model.Field;
 import de.techfak.gse.ysander.model.State;
@@ -16,10 +24,20 @@ import de.techfak.gse.ysander.model.error.InvalidFieldException;
 public class ChessTile extends AnchorPane {
 
     private final ReadOnlyIntegerWrapper y = new ReadOnlyIntegerWrapper();
+
     private final ReadOnlyIntegerWrapper x = new ReadOnlyIntegerWrapper();
+
     private final ObjectProperty<State> state = new SimpleObjectProperty<>();
 
+    private final ObjectProperty<Field> onClick = new SimpleObjectProperty<>();
+
     private final Field fieldKey;
+
+    @FXML
+    private Button tileInput;
+
+    @FXML
+    private AnchorPane background;
 
 
     public ChessTile(@NamedArg("y") int y, @NamedArg("x") int x) {
@@ -31,6 +49,7 @@ public class ChessTile extends AnchorPane {
 
         this.y.set(y);
         this.x.set(x);
+
 
         try {
             this.fieldKey = new Field(x, y);
@@ -47,6 +66,18 @@ public class ChessTile extends AnchorPane {
     }
 
 
+    @FXML
+    public void initialize() {
+
+        this.background.setBackground(new Background(new BackgroundFill(
+            ((getY() + getX()) % 2 == 0
+             ? Color.DARKGRAY
+             : Color.LIGHTGRAY), CornerRadii.EMPTY, Insets.EMPTY
+
+        )));
+
+        this.tileInput.setOnMouseClicked(event -> this.onClick.setValue(this.fieldKey));
+    }
 
 
     // accessor
@@ -69,7 +100,17 @@ public class ChessTile extends AnchorPane {
     public State getState() {
         return state.get();
     }
+
     public ObjectProperty<State> stateProperty() {
         return state;
     }
+
+    public Field getOnClick() {
+        return onClick.get();
+    }
+
+    public ObjectProperty<Field> onClickProperty() {
+        return onClick;
+    }
+
 }
--- src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessUIController.java
+++ src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessUIController.java
@@ -1,41 +1,56 @@
 package de.techfak.gse.ysander.view.gui.elements;
 
-import java.net.URL;
-import java.util.Observable;
-import java.util.ResourceBundle;
-
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+
 import javafx.scene.layout.GridPane;
 
+import de.techfak.gse.ysander.communication.handlers.FieldInputHandler;
+import de.techfak.gse.ysander.communication.inputs.FieldInput;
 import de.techfak.gse.ysander.communication.output.Output;
 import de.techfak.gse.ysander.model.State;
 
-public class ChessUIController implements Output<State>{
+public class ChessUIController implements Output<State>, FieldInput {
 
     @FXML
     private GridPane grid;
     private ObjectProperty<State> state = new SimpleObjectProperty<>();
 
+    private FieldInputHandler fieldInputHandler = (f) -> {System.out.println(f);};
+
 
     @FXML
     public void initialize() {
         this.grid.getChildren().stream()
-            .filter(n -> n instanceof Button)
-            .map(n -> (Button) n)
+            .filter(n -> n instanceof ChessTile)
+            .map(n -> (ChessTile) n)
+            .forEach(tile -> {
+                tile.stateProperty().bind(state);
+                tile.onClickProperty().addListener((observable, old, current) -> {
+                    this.fieldInputHandler.handleFieldInput(current);
+                });
+
+            });
+
+
+
+/*
+
             .forEach(tile -> tile.setOnMouseClicked(mouseEvent -> {
                 int y  = GridPane.getRowIndex(tile);
                 int x = GridPane.getColumnIndex(tile);
             }));
+            */
     }
 
     @Override
     public void display(final State state) {
         this.state.setValue(state);
+    }
+
+    @Override
+    public void setFieldInputHandler(final FieldInputHandler fieldInputHandler) {
+        this.fieldInputHandler = fieldInputHandler;
     }
 }
