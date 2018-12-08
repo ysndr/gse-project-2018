92f419a49b9a9fde199d0389605f48e6d9cad3cc
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-08 01:15:44 +0100

    Show current player indicator (#54398, #54389, @0h15m)

--- src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessUIController.java
+++ src/main/java/de/techfak/gse/ysander/view/gui/elements/ChessUIController.java
@@ -10,6 +10,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 import de.techfak.gse.ysander.communication.handlers.FieldInputHandler;
@@ -17,12 +19,16 @@ import de.techfak.gse.ysander.communication.inputs.FieldInput;
 import de.techfak.gse.ysander.communication.output.Output;
 import de.techfak.gse.ysander.model.Field;
 import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.figures.Figure;
 import de.techfak.gse.ysander.view.View;
 
 public class ChessUIController implements View, FieldInput {
 
     @FXML
     private GridPane grid;
+    @FXML
+    private ImageView currentPlayerIndicator;
+
     private ObjectProperty<State> state = new SimpleObjectProperty<>();
 
     private Runnable onInitCB;
@@ -44,6 +50,19 @@ public class ChessUIController implements View, FieldInput {
                     inputHandler.handleFieldInput(t1);
                 });
             });
+
+        this.stateProperty().addListener((observableValue, state1, t1) -> {
+            if (t1.getColor() == null) {
+                return;
+            }
+
+            if (t1.getColor() == Figure.Color.WHITE) {
+                currentPlayerIndicator.setImage(new Image("/de/techfak/gse/ysander/model/figures/king_white.png"));
+            } else {
+                currentPlayerIndicator.setImage(new Image("/de/techfak/gse/ysander/model/figures/king_black.png"));
+            }
+
+        });
     }
 
     public State getState() {
