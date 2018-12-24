002d2a8e18b0d6d0d35f6a89093c04bd48a1e9f4
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-24 17:33:53 +0100

    Make HintProviders chainable and cachable (refs: #58609) @0h45m

---
+++ src/main/java/de/techfak/gse/ysander/model/rules/providers/ChainedHintProvider.java
@@ -0,0 +1,49 @@
+package de.techfak.gse.ysander.model.rules.providers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.rules.Hint;
+
+public class ChainedHintProvider implements HintProvider {
+
+    private final HintProvider wrapped;
+    private ChainedHintProvider tail;
+
+    public ChainedHintProvider(final HintProvider wrapped) {
+        this.wrapped = wrapped;
+    }
+
+
+    /**
+     * Chain more {@link HintProvider}s.
+     * Creates a simple singly linked list of {@link ChainedHintProvider}s by
+     * managing a tail of another {@link ChainedHintProvider} which might contain a
+     * tail itself or not recursing further.
+     * @param other the other {@link HintProvider} to chain up
+     * @return new {@link ChainedHintProvider} which has the current one as its tail.
+     */
+    @Override
+    public ChainedHintProvider chain(final HintProvider other) {
+        ChainedHintProvider extended = new ChainedHintProvider(other);
+        extended.tail = this;
+        return extended;
+    }
+
+
+    /**
+     * Creates a set of rules by unifying its wrapped rulesets rules with its
+     * tails wrapped rulesets rules...
+     * @param state the state to create rules for
+     * @return A combined set of rules contained in the chain
+     */
+    @Override
+    public Set<Hint> getHints(final State state) {
+        Set<Hint> hints = new HashSet<>(wrapped.getHints(state));
+        if (tail != null) {
+            hints.addAll(tail.getHints(state));
+        }
+        return hints;
+    }
+}
---
+++ src/test/java/de/techfak/gse/ysander/model/rules/providers/ChainedHintProviderTest.java
@@ -0,0 +1,68 @@
+package de.techfak.gse.ysander.model.rules.providers;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import de.techfak.gse.ysander.model.Field;
+import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.rules.Hint;
+import de.techfak.gse.ysander.util.Cache;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChainedHintProviderTest {
+
+    @Test
+    void chain() {
+        AtomicInteger counter = new AtomicInteger();
+
+        HintProvider A = createHintPrivider(0,2, counter);
+        HintProvider B = createHintPrivider(1, 0, counter);
+
+        A.chain(B).getHints(null);
+
+        assertEquals(12, counter.get());
+
+        counter.set(0);
+        Cache<Set<Hint>> C = A.chain(B).getHintsCached(null);
+        C.result();
+        C.result(); // should be cached here;
+
+
+        assertEquals(12, counter.get());
+
+
+
+
+
+
+    }
+
+    private HintProvider createHintPrivider(int x ,int y, AtomicInteger counter) {
+        return (final State state) -> {
+
+                Set<Hint> set = new HashSet<>();
+                counter.addAndGet(x*10 + y);
+                set.add(new Hint() {
+                    @Override
+                    public Field target() {
+                        return new Field(x, y);
+                    }
+
+                    @Override
+                    public State apply(final State state) {
+
+                        return state;
+                    }
+                });
+
+                return set;
+            };
+    }
+
+    @Test
+    void getHints() {
+    }
+}
