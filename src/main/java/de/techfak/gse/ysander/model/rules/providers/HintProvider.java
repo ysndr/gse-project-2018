abf171490b34bae9f320800f9a5eb3f91f91e22f
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-24 17:05:14 +0100

    Add HintProvider Interface (refs: #58609) @0h30m

---
+++ src/main/java/de/techfak/gse/ysander/model/rules/providers/HintProvider.java
@@ -0,0 +1,27 @@
+package de.techfak.gse.ysander.model.rules.providers;
+
+import java.util.Set;
+
+import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.rules.Hint;
+import de.techfak.gse.ysander.util.Cache;
+
+public interface HintProvider {
+    /**
+     * Computes rules from a given state.
+     * The kind of rules returned is dependent on the Implementor and the state.
+     * @param state the state to create rules for
+     * @return a set of rules
+     */
+    Set<Hint> getHints(State state);
+
+    default Cache<Set<Hint>> getHintsCached(State state) {
+        return Cache.cache(() -> this.getHints(state));
+    };
+
+    default ChainedHintProvider chain(HintProvider other) {
+        return new ChainedHintProvider(this).chain(other);
+    }
+
+
+}
