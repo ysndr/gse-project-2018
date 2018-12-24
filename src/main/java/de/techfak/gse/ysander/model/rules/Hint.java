b7a0c79b4a9b23213b6aaeeec1408ed3170317b2
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-24 16:41:15 +0100

    Introduce the Hint interface (refs: #58609) @0h15m

---
+++ src/main/java/de/techfak/gse/ysander/model/rules/Hint.java
@@ -0,0 +1,25 @@
+package de.techfak.gse.ysander.model.rules;
+
+import de.techfak.gse.ysander.model.Field;
+import de.techfak.gse.ysander.model.State;
+
+/**
+ * A {@code Hint} is a tool to change the current {@link State}.
+ *
+ * - Hints are weakly bound to a specific target {@link Field} which, when selected,
+ *   should initiate the application of the {@code Hint} onto the state.
+ *
+ * - Hints are specific to a state and computed independently for each state.
+ *
+ * - The task of coordinating the application if hints is left to Controllers
+ *   on higher levels.
+ */
+public interface Hint {
+    /**
+     * The field this Hint is bound to. It should only be applies when this
+     * field is chosen
+     * @return {@link Field}
+     */
+    Field target();
+    State apply(State state);
+}
