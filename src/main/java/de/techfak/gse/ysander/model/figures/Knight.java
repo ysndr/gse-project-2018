48d5f6e59b909d826a69e04fa51dc9737d6e46c3
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2018-12-26 17:11:26 +0100

    Implement MoveHint Providing for Knight (refs: #58009) @0h15m

--- src/main/java/de/techfak/gse/ysander/model/figures/Knight.java
+++ src/main/java/de/techfak/gse/ysander/model/figures/Knight.java
@@ -1,6 +1,11 @@
 package de.techfak.gse.ysander.model.figures;
 
 import java.util.Objects;
+import java.util.Set;
+
+import de.techfak.gse.ysander.model.State;
+import de.techfak.gse.ysander.model.rules.Hint;
+import de.techfak.gse.ysander.model.rules.providers.MoveHintProvider;
 
 /**
  * The Knight figures.
@@ -27,6 +32,19 @@ public class Knight extends Figure {
     @Override
     public String canonicalName() {
         return NAME;
+    }
+
+    @Override
+    public Set<? extends Hint> getHints(final State state) {
+        return new MoveHintProvider(-2, 1, 1)
+            .chain(new MoveHintProvider(-2, -1, 1))
+            .chain(new MoveHintProvider(-1, 2, 1))
+            .chain(new MoveHintProvider(-1, -2, 1))
+            .chain(new MoveHintProvider(2, 1, 1))
+            .chain(new MoveHintProvider(2, -1, 1))
+            .chain(new MoveHintProvider(1, 2, 1))
+            .chain(new MoveHintProvider(1, -2, 1))
+            .getHints(state);
     }
 
     @Override
---
+++ src/test/java/de/techfak/gse/ysander/model/figures/KnightTest.java
@@ -0,0 +1,77 @@
+package de.techfak.gse.ysander.model.figures;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import de.techfak.gse.ysander.model.*;
+import de.techfak.gse.ysander.model.rules.Hint;
+import de.techfak.gse.ysander.model.rules.MoveHint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class KnightTest {
+
+
+    private State state;
+
+    @BeforeEach
+    void setUp() {
+        state = StateBuilder.defaultState().builder()
+            .setGrid(GridBuilder.fromFEN("8/8/8/8/8/8/8/8")
+                         .builder()
+                         .setField(new Field(3, 3), new Knight(Figure.Color.WHITE))
+                         .createGrid())
+            .setSelection(new Field(3, 3))
+            .createState();
+    }
+
+    @Test
+    void getHintsUnblocked() {
+        Set<Hint> hints = Stream.of(
+            new Field(1, 2),
+            new Field(2, 1),
+            new Field(5, 4),
+            new Field(4, 5),
+            new Field(1, 4),
+            new Field(2, 5),
+            new Field(5, 2),
+            new Field(4, 1))
+            .map(to -> new Move(new Field(3, 3), to))
+            .map(MoveHint::new)
+            .collect(Collectors.toSet());
+
+        assertEquals(hints, new Knight(Figure.Color.BLACK).getHints(state));
+    }
+
+    @Test
+    void getHintsBlocked() {
+
+        Set<Hint> hints = Stream.of(
+            new Field(1, 2),
+            new Field(5, 4),
+            new Field(4, 5),
+            new Field(1, 4),
+            new Field(2, 5),
+            new Field(5, 2),
+            new Field(4, 1))
+            .map(to -> new Move(new Field(3, 3), to))
+            .map(MoveHint::new)
+            .collect(Collectors.toSet());
+
+        state = state.builder()
+            .setGrid(state.getGrid().builder()
+                         .setField(new Field(2, 1), new Knight(Figure.Color.BLACK))
+                         .createGrid())
+            .createState();
+
+        assertEquals(hints, new Knight(Figure.Color.BLACK).getHints(state));
+
+    }
+
+
+
+}
