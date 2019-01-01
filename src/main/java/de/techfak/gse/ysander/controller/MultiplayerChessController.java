f889206e46c46be4e880917fb0809eeafbc38708
Yannik Sander <ysander@techfak.uni-bielefeld.de>
2019-01-01 16:23:02 +0100

    Modify Controller to roperly extend the base (refs.: #58001) @0h05m

--- src/main/java/de/techfak/gse/ysander/controller/MultiplayerChessController.java
+++ src/main/java/de/techfak/gse/ysander/controller/MultiplayerChessController.java
@@ -1,12 +1,13 @@
 package de.techfak.gse.ysander.controller;
 
 import de.techfak.gse.ysander.communication.drivers.PlayerInputDriver;
+import de.techfak.gse.ysander.communication.handlers.ErrorHandler;
 import de.techfak.gse.ysander.communication.handlers.PlayerInputHandler;
-import de.techfak.gse.ysander.model.Field;
 import de.techfak.gse.ysander.model.Player;
 import de.techfak.gse.ysander.model.State;
 import de.techfak.gse.ysander.model.error.InvalidMoveException;
 import de.techfak.gse.ysander.model.figures.Figure;
+import de.techfak.gse.ysander.model.rules.Hint;
 import de.techfak.gse.ysander.view.View;
 
 /**
@@ -20,50 +21,46 @@ public class MultiplayerChessController extends BaseChessController implements P
     private final PlayerInputDriver playerBlack;
 
 
-    public MultiplayerChessController(final View view,
-                                      final State initialState,
+    public MultiplayerChessController(final State initialState, final View view,
+                                      final ErrorHandler errorHandler,
                                       final PlayerInputDriver playerWhite,
                                       final PlayerInputDriver playerBlack) {
-        super(view, initialState);
+        super(initialState, view, errorHandler);
+
 
         this.playerWhite = playerWhite;
         this.playerBlack = playerBlack;
 
-        view.setOnInitCB(() -> view.display(initialState));
         playerWhite.setPlayerInputHandler(this);
         playerBlack.setPlayerInputHandler(this);
 
-        this.setState(initialState);
+        this.updateState(state);
+
     }
 
-
     @Override
-    void setState(final State state) {
-        super.setState(state);
+    protected void updateState(final State state) {
+        super.updateState(state);
+
         if (this.state.getColor().equals(Figure.Color.WHITE)) {
             playerWhite.startTurn(this.state);
         } else if (this.state.getColor().equals((Figure.Color.BLACK))) {
             playerBlack.startTurn(this.state);
         }
-
     }
 
     @Override
-    public void handlePlayerInput(final Field input, final Player player) {
+    public void handlePlayerInput(final Hint input, final Player player) {
         State newState = state;
         try {
-            newState = state.applyField(input);
+            newState = state.applyHint(input);
         } catch (InvalidMoveException e) {
-            this.handleError(e);
+            errorHandler.handleError(e);
         }
 
-        if (newState != null && newState.getColor() == Figure.Color.WHITE) {
-            playerWhite.startTurn(newState);
-        } else {
-            playerBlack.startTurn(newState);
-        }
-        super.setState(newState);
-        output.display(newState);
+        this.updateState(newState);
+
     }
+
 
 }
